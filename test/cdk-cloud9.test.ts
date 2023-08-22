5cdb58fd0069aafc72d49d12d458e0578b93f122
olcortesb <olcortesb@gmail.com>
2023-08-22 00:49:48 -0300

    Add first commit

---
+++ test/cdk-cloud9.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCloud9 from '../lib/cdk-cloud9-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-cloud9-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCloud9.CdkCloud9Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
